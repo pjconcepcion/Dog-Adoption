52ec8a3f12a6f3632f0c6e64683bd2622f8bdf99
PJ Concepcion <patrickjosephconcepcion@gmail.com>
2018-10-25 19:05:55 +0800

    db connection

--- resources/views/layout/customerApp.blade.php
+++ resources/views/layout/customerApp.blade.php
@@ -4,7 +4,7 @@
         <title>FURRYTAILS</title>
         <link rel="icon" href="image/paw.ico">
         <meta charset="utf-8">
-        <meta name="csrf-token" content="{{csrf_token(">
+        <meta name="csrf-token" content="{{csrf_token()}}">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800" rel="stylesheet">
