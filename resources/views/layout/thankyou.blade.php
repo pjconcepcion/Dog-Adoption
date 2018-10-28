0b377065daad55b3309e24380d89a6b527a0c88d
PJ Concepcion <patrickjosephconcepcion@gmail.com>
2018-10-29 06:01:22 +0800

    finalize

--- resources/views/layout/thankyou.blade.php
+++ resources/views/layout/thankyou.blade.php
@@ -9,16 +9,16 @@
 
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800" rel="stylesheet">
 
-        <link rel="stylesheet" href="../customer/css/bootstrap.css">
-        <link rel="stylesheet" href="../customer/css/animate.css">
-        <link rel="stylesheet" href="../customer/css/owl.carousel.min.css">
-        <link rel="stylesheet" href="../customer/fonts/ionicons/css/ionicons.min.css">
-        <link rel="stylesheet" href="../customer/fonts/fontawesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../../customer/css/bootstrap.css">
+        <link rel="stylesheet" href="../../customer/css/animate.css">
+        <link rel="stylesheet" href="../../customer/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="../../customer/fonts/ionicons/css/ionicons.min.css">
+        <link rel="stylesheet" href="../../customer/fonts/fontawesome/css/font-awesome.min.css">
       
         {{-- <link rel="stylesheet" href="customer/fonts/flaticon/font/flaticon.css"> --}}
 
         <!-- Theme Style -->
-        <link rel="stylesheet" href="../customer/css/style.css">
+        <link rel="stylesheet" href="../../customer/css/style.css">
     </head>
   
     <body>
@@ -164,12 +164,12 @@
   <!-- loader -->
   <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
 
-  <script src="../customer/js/jquery-3.2.1.min.js"></script>
-  <script src="../customer/js/popper.min.js"></script>
-  <script src="../customer/js/bootstrap.min.js"></script>
-  <script src="../customer/js/owl.carousel.min.js"></script>
-  <script src="../customer/js/jquery.waypoints.min.js"></script>
-  <script src="../customer/js/main.js"></script>
+  <script src="../../customer/js/jquery-3.2.1.min.js"></script>
+  <script src="../../customer/js/popper.min.js"></script>
+  <script src="../../customer/js/bootstrap.min.js"></script>
+  <script src="../../customer/js/owl.carousel.min.js"></script>
+  <script src="../../customer/js/jquery.waypoints.min.js"></script>
+  <script src="../../customer/js/main.js"></script>
   <script> 
     var url = window.location.href;
     var route = url.split('/');
